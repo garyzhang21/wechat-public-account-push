5a13f3aa5cfdbde31655bcec2026b1c5cbf93e1d
garyzhang21 <32188719+garyzhang21@users.noreply.github.com>
2023-02-12 21:01:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,17 +5,17 @@ export const config = {
     // 公众号appSecret
     appSecret: "11818ff748613cf59797c71ef24d6e86",
     // 模板消息id
-    templateId: "nYI49VHm5rRJjXoMmWcYE3xhsLEXFTTw8Zr970Os-aU",
+    templateId: "v4DESFogd7vJoPhE_euzNh7ZpmididPmwLbnmCikMTE",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [""],
+    user: ["oK6sm6JSqTn-l5qxjpgk8NoRYHXE"],
      
     // 信息配置
     // 所在省份
     province: "辽宁",
     // 所在城市
-    city: "沈阳",
+    city: "大连",
     // 相遇的日子
-    loveDate: "2022-11-11",
+    loveDate: "2022-12-11",
     // 上次见面已经过了
     }
 
